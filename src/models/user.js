21b270606c6f5d11c4133fbbcf58f906d1ce21d1
Sukumar Reddy B <sukumar1118@gmail.com>
2025-01-08 17:22:51 +0530

    Schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,22 +1,49 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/man-user-color-icon.png",
+    },
+    about: {
+      type: String,
+      required: true,
+      default: "I am a software developer",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
